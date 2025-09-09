1a1ab348eb7f2e0c4c02567ca932337bde644e98
natasha-chen <natashachen44@gmail.com>
2025-09-09 12:04:03 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -3,7 +3,7 @@ package lab1;
 /**
  * Solve the lab1.FizzBuzz challenge.
  */
-class FizzBuzz {
+/** class FizzBuzz {
 
     public static void main(String[] args) {
 
@@ -32,5 +32,44 @@ class FizzBuzz {
 
             }
         }
+    }
+}
+
+ */
+
+class FizzBuzz {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 100) {
+            i = doFizzBuzz(i);
+        }
+    }
+
+    private static int doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 }
